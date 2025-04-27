bfce3ae7290ddb72ad8142c839eb84ecf3d7662d
choihyeonseo38 <porroriiiii11@gmail.com>
2025-04-27 22:49:34 +0900

    Add webstart folder

--- index.js
+++
@@ -1,8 +0,0 @@
-let server = require('./server');
-let router = require('./router');
-let requesthandler = require('./requesthandler');
-
-const mariadb = require('./database/connect/mariadb');
-mariadb.connect();
-
-server.start(router.route, requesthandler.handle);
